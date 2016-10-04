4ae898aba6b31510f987dbc865e9a800ac29aea7
Andres Huertas S <afhuertass@unal.edu.co>
2016-10-04 00:19:30 -0500

    Ready the script for the calculation of the principal components next steps order de data an analysing it

--- src/ssa.py
+++ src/ssa.py
@@ -11,18 +11,30 @@ import matplotlib.pyplot as plt
 dataFolder = "../data/"
 dataFile =  dataFolder + "series1.xlsx"
 
-sheetName = "Corabastos1"
+sheetName = "Corabastos2"
 ## abrir archivo
 
 xFile = pd.ExcelFile(dataFile)
 dataFrame = xFile.parse(sheetName)
 
-M = 10  # lags
+M = 4  # lags
 
 correlationList = []
 
+datosNumpy = dataFrame["Datos"].as_matrix()
+size = datosNumpy.size
+
+
+
+
 for lag in range(0, M):
     correlationList.append( float( dataFrame["Datos"].autocorr(lag) ) )
+    if lag >= 1:
+        datosNumpy2 = dataFrame["Datos"].as_matrix()
+        datosNumpy2 = np.roll( datosNumpy2 , lag )
+        datosNumpy = np.concatenate( (datosNumpy ,  datosNumpy2) , axis=0  ) 
+    
+
 
 correlationVector = np.array(correlationList)    
 #cov = dataFrame["Datos"].autocorr(1) #.corr( dataFrame["Datos"])
@@ -31,4 +43,16 @@ correlationVector = np.array(correlationList)
 CovarianceMatrix = toeplitz (correlationVector )
 
 w,v = np.linalg.eig( CovarianceMatrix )
-print (  v  )
+
+
+#plt.show()
+
+datosNumpy = np.reshape( datosNumpy , (  M , size ) )
+
+PC = np.matmul( datosNumpy.T  , v ) # 
+
+print (  datosNumpy )
+print ( v.shape )
+
+plt.plot( PC )
+plt.show()
