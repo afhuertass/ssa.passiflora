1eaabf346eb1e8ff418f9c065ebdf2c7f9dd40c1
Andres Huertas S <afhuertass@unal.edu.co>
2016-10-07 00:15:30 -0500

    Organized. Next step more general data input and fancy looks for the graphs

--- src/ssa.py
+++ src/ssa.py
@@ -27,7 +27,7 @@ def CovarianceMatrix( dataSerie , M ):
 
     correlationNp = np.array(correlationList)
 
-    return toeplitz (correlationVector ) # return covariance matrix 
+    return toeplitz (correlationNp ) # return covariance matrix 
     
 
 def YMatrix ( dataSerie , M ) :
@@ -50,7 +50,7 @@ def YMatrix ( dataSerie , M ) :
 def principalComponents( serieDatos , M  ):
 
     CovMatrix = CovarianceMatrix(serieDatos , M)
-    Ydata = YMatrix( dataSerie , M)
+    Ydata = YMatrix( serieDatos , M)
 
     EigenVals , EigenVecs = np.linalg.eig( CovMatrix )
 
@@ -62,7 +62,7 @@ def principalComponents( serieDatos , M  ):
 def reconstructedComponents(  Pcs , index , M , EigenVecs ):
 
     Z = calculateZ ( Pcs , index , M)
-    RC1 = np.matmul( Z  , Eigenvecs[:][index].T   )/M
+    RC1 = np.matmul( Z  , EigenVecs[:][index].T   )/M
 
     return RC1
     
@@ -75,7 +75,7 @@ def mainProgram( serieDatos , M ):
 
     RC1 = reconstructedComponents (  PCS , 0 , M , EigenVs )
 
-    
+    return PCS , RC1
     
     
     
@@ -94,76 +94,12 @@ M = 4  # lags
 correlationList = []
 
 datosNumpy = dataFrame["Datos"].as_matrix()
-datosNumpy = datosNumpy # columna
-
-datosOriginal = datosNumpy
-size = datosNumpy.size
 
 
+PC , Rc1 = mainProgram( datosNumpy , M)
+  
 
-
-for lag in range(0, M):
-    correlationList.append( float( dataFrame["Datos"].autocorr(lag) ) )
-    if lag >= 1:
-        datosNumpy2 = dataFrame["Datos"].as_matrix()
-        datosNumpy2 = np.roll( datosNumpy2 , -lag )
-        #datosNumpy2[0:lag] = 0
-        datosNumpy2[ (size-lag) :(size) ] = 0
-        print(datosNumpy2)
-        datosNumpy = np.concatenate( (datosNumpy ,  datosNumpy2)   ) 
-    
-datosNumpy = datosNumpy.reshape( M , size ).T
-print( "Matriz Y" )
-print (datosNumpy) # matriz Y del paper 
-
-correlationVector = np.array(correlationList)    
-#cov = dataFrame["Datos"].autocorr(1) #.corr( dataFrame["Datos"])
-
-## correlation matrix 
-CovarianceMatrix = toeplitz (correlationVector )
-
-
-w,v = np.linalg.eig( CovarianceMatrix )
-
-
-
-#plt.show()
-
-
-
-PC = np.matmul( datosNumpy  , v.T ) # 
-print("Componentes principales")
-print( PC)
-print( "Primera componente principal")
-print( PC.T[0][:] )
-
-#  Construir
-PCIndex = 0 ; ## este numero cambiara con
-
-
-Z1 = calculateZ( PC , 0 , M)
-Z2 = calculateZ( PC , 1 , M)
-Z3 = calculateZ(PC , 2 , M)
-Z4 = calculateZ(PC , 3 , M)
-
-print("Z4")
-print( Z4 ) 
-
-RC1 = np.matmul( Z1 , v[:][0].T)/M
-RC2 = np.matmul(Z2 , v[:][1].T)/M
-RC3 = np.matmul(Z3 , v[:][2].T)/M
-RC4 = np.matmul(Z4 , v[:][3].T)/M
-
-RC = RC1 + RC2 + RC3 + RC4
-print("Reconstructed")
-print(RC1)
-
-
-
-plt.plot( (RC1 ) )
-plt.plot( (RC2 ) )
-#plt.plot( (RC3 ) )
-#plt.plot( (RC ) )
-plt.plot( datosOriginal )
+plt.plot( Rc1  )
+plt.plot( datosNumpy )
 
 plt.show()
